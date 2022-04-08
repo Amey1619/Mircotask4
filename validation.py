ce9edf399777eb2f1b58a59f3350242e5fb783da
Amey gupta <89736812+Amey1619@users.noreply.github.com>
2022-04-08 11:50:21 +0530

    Update validation.py

--- validation.py
+++ validation.py
@@ -1,7 +1,7 @@
 import re
 article = input("Enter your URL: ")
 
-valid1=re.match('https://[a-zA-Z0-9.]+/.+',article)
+valid1=re.match('http:|https://[a-zA-Z0-9.]+/.+',article)
 valid2=re.search('https://www.google.com/search/|https://www.facebook.com/|HTTPS://WWW.GOOGLE.COM/SEARCH|HTTPS://WWW.FACEBOOK.COM',article)
 
 if(valid1!=None):
