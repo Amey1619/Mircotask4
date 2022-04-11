a8b5d39ac0af46eed2ed09e0871b74b20ea94ac7
Amey gupta <89736812+Amey1619@users.noreply.github.com>
2022-04-11 21:51:58 +0530

    Update validation.py

--- validation.py
+++ validation.py
@@ -1,5 +1,13 @@
 import re
+import requests
 article = input("Enter your URL: ")
+
+def validations(article):
+  valid3=requests.get(article)
+  if(valid3.status_code==200):
+    print(" Valid URL ")
+  else:
+    print(" Invalid URL ")
 
 valid1=re.match('http:|https://[a-zA-Z0-9.]+/.+',article)
 valid2=re.search('https://www.google.com/search/|https://www.facebook.com/|HTTPS://WWW.GOOGLE.COM/SEARCH|HTTPS://WWW.FACEBOOK.COM',article)
@@ -8,7 +16,7 @@ if(valid1!=None):
   if(valid2!=None):
     print(" Invalid URL ")
   else:
-    print(" valid URL ")
+    validations(article)
 else:
   print("Invalid URL")
  
