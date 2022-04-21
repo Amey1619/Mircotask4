a420f7caff8dccb25308c4157e977b37251b1e1d
Amey gupta <89736812+Amey1619@users.noreply.github.com>
2022-04-21 11:33:52 +0530

    Update validation.py

--- validation.py
+++ validation.py
@@ -3,20 +3,20 @@ import requests
 article = input("Enter your URL: ")
 
 def validations(article):
-  valid3=requests.get(article)
-  if(valid3.status_code==200):
+  valid3=requests.get(article) # valid3 will send a get request and validates the URL
+  if(valid3.status_code==200): # If status_code returns 200 it will a valid URL otherwise it not.
     print(" Valid URL ")
   else:
     print(" Invalid URL ")
 
-valid1=re.match('http:|https://[a-zA-Z0-9.]+/.+',article)
-valid2=re.search('https://www.google.com/search/|https://www.facebook.com/|HTTPS://WWW.GOOGLE.COM/SEARCH|HTTPS://WWW.FACEBOOK.COM',article)
+valid1=re.match('http:|https://[a-zA-Z0-9.]+/.+',article)   # Matches is URL matches the format of URL
+valid2=re.search('https://www.google.com/search/|https://www.facebook.com/|HTTPS://WWW.GOOGLE.COM/SEARCH|HTTPS://WWW.FACEBOOK.COM',article) # Avoiding google and facebook unreliable URL
 
-if(valid1!=None):
-  if(valid2!=None):
+if(valid1!=None): # If valid1 does not match then format is incorrect 
+  if(valid2!=None): # If valid2 does not find any google search results it is correct
     print(" Invalid URL ")
   else:
-    validations(article)
+    validations(article) # If valid1 & valid2 fields are cleared then only validations function will run !
 else:
   print("Invalid URL")
  
